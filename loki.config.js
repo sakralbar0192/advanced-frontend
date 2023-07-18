2e5dc890ddd680a10eee78d996ef0f8bca037bf8
DUkhov-book <sakralbar0192@gmail.com>
2023-07-18 09:06:16 +0800

    remove loki folder

--- loki.config.js
+++ loki.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     chromeSelector: "#storybook-root > *",
-    diffingEngine: 'pixelmatch',
+    diffingEngine: 'looks-same',
     configurations: {
       'chrome.laptop': {
         target: 'chrome.app',
